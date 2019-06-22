6996dd5843bf970935ac9d57809e022810753c27
Gregory Giordano <greggior@Gregorys-iMac.local>
2019-06-22 17:16:30 -0600

    hello world, flask

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=kismet.py
+
--- js/project.js
+++
@@ -1,5 +0,0 @@
-function getFortune(){
-	document.getElementById("wholeCookie").style.display = "none";
-	document.getElementById("cookie").innerHTML = "<img src='images/Fortune_cookie_broken.jpg'/>";
-	document.getElementById("fortune").innerHTML = "The Class will get an A!";
-}
