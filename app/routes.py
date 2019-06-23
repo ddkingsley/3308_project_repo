4d79369170385b6975c1cc7d1abf8bf89fabc951
Gregory Giordano <greggior@Gregorys-iMac.local>
2019-06-22 19:06:13 -0600

    moving between pages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
+from flask import url_for
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'The Class'}   
+    return render_template('index.html', user=user)
+
+@app.route('/cookie/')
+def cookie():
+    return render_template('cookie.html')
