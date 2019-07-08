03add9f9d30655f3585232bebe9b3349291a211e
Gregory Giordano <greggior@rgnt1-63-136-dhcp.int.colorado.edu>
2019-07-08 12:22:41 -0600

    updated path to fortunes text file

--- app/functions/cookie.py
+++ app/functions/cookie.py
@@ -1,6 +1,6 @@
 import random
 
 def getAFortune():
-	fortuneList = open("Fortune_data.txt").readlines()
+	fortuneList = open("app/functions/Fortune_data.txt").readlines()
 	fortune = random.choice(fortuneList)
 	return fortune
