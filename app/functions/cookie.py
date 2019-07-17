2bd86128e107b8f9c580e922c00e2118a5d66778
Gregory Giordano <greggior@rgnt1-62-240-dhcp.int.colorado.edu>
2019-07-17 10:49:39 -0600

    login round 1

--- app/functions/cookie.py
+++ app/functions/cookie.py
@@ -4,3 +4,7 @@ def getAFortune():
 	fortuneList = open("app/functions/Fortune_data.txt").readlines()
 	fortune = random.choice(fortuneList)
 	return fortune
+
+def getALuckyNumber():
+	luckyNumber = random.randint(1,100)
+	return luckyNumber
