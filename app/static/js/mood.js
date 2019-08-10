81d4f08c8582da974d1886f1f1ab379233917eb0
kristin-kernler <34021742+kristin-kernler@users.noreply.github.com>
2019-08-09 18:38:34 -0600

    Update mood.js

--- app/static/js/mood.js
+++ app/static/js/mood.js
@@ -16,69 +16,134 @@ window.onload = function() {
 }
 
 function get_mood() {
-    const answer = document.getElementById("colorText");
+    var answer = document.getElementById("colorText");
 
     answer.textContent = generate_color()
-    meaning = document.getElementById("colorMeaning");
+    meaning = document.getElementById("colorMeaning")
+
+    var defaultRing = document.getElementById("default");
+    defaultRing.style.display = "none";
+
+    var originalButton = document.querySelector(".checkButton");
+    originalButton.style.display = "none";
+
+    var tryAgainButton = document.querySelector(".resetButton");
+    tryAgainButton.style.display = "block";
+
+    tryAgainButton.addEventListener('click', reset);
 
     // Black
     if (answer.textContent == ringColors[0])
     {
+        var blackRing = document.getElementById("black");
+        blackRing.style.display = "block"; 
         meaning.textContent = "You feel angry, fearful, or apathetic.";
     }
     // Yellow
     if (answer.textContent == ringColors[1])
     {
+        var yellowRing = document.getElementById("yellow");
+        defaultRing.style.display = "block";
         meaning.textContent = "Your emotions are mixed or conflicted. You may feel distracted or slightly anxious.";
     }
     // Orange
     if (answer.textContent == ringColors[2])
     {
+        var orangeRing = document.getElementById("orange");
+        orangeRing.style.display = "block";
         meaning.textContent = "You are upset. You feel nervous, stressed, or confused about something.";
     }
     // Light Green
     if (answer.textContent == ringColors[3])
     {
+        var lGreenRing = document.getElementById("lGreen");
+        lGreenRing.style.display = "block";
         meaning.textContent = "You are anxious about something or someone. You are guarded and alert. You may feel mild jealousy or stress.";
     }
     // Green
     if (answer.textContent == ringColors[4])
     {
+        var greenRing = document.getElementById("green");
+        greenRing.style.display = "block";
         meaning.textContent = "This is the default color. Your emotions are stable today: you do not feel execessive negativity or positivity.";
     }
     // Blue
     if (answer.textContent == ringColors[5])
     {
+        var blueRing = document.getElementById("blue");
+        blueRing.style.display = "block";
         meaning.textContent = "You are cheerful and optimistic.";
     }
     // Dark Blue
     if (answer.textContent == ringColors[6])
     {
+        var dBlueRing = document.getElementById("dBlue");
+        dBlueRing.style.display = "block";
         meaning.textContent = "You are content.";
     }
     // Violet
     if (answer.textContent == ringColors[7])
     {
+        var violetRing = document.getElementById("violet");
+        violetRing.style.display = "block";
         meaning.textContent = "You have romance on your mind. You feel moody, mischievous, or sensual. You are prone to unpredictable choices today.";
     }
     // Pink
     if (answer.textContent == ringColors[8])
     {
+        var pinkRing = document.getElementById("pink");
+        pinkRing.style.display = "block";
         meaning.textContent = "You are overcome with strong emotions. You are infatuated with someone or something.";
     }
+
 }
 
-//function reset() {
-    //const answer = document.getElementById("colorText");
-    //answer.textContent = "";
-    //const meaning = document.getElementById("colorMeaning");
-    //meaning.textContent = "";
-    //document.body.removeChild(next)
-    //answerButton = document.createElement('button')
-    //answerButton.innerHTML = "Check Ring";
-    //document.body.appendChild(answerButton)
-    //answerButton.addEventListener('click', get_mood)
-  //}
+function reset() {
+    var answer = document.getElementById("colorText");
+    answer.textContent = "";
+
+
+    var meaning = document.getElementById("colorMeaning");
+    meaning.textContent = "";
+
+    var defaultColor = document.getElementById("default");
+    defaultColor.style.display = "block";
+
+    var originalButton = document.querySelector(".checkButton");
+    originalButton.style.display = "block";
+
+    var tryAgainButton = document.querySelector(".resetButton");
+    tryAgainButton.style.display = "none";
+
+    var oldblackRing = document.getElementById("black");
+    oldblackRing.style.display = "none"; 
+
+    var oldyellowRing = document.getElementById("yellow");
+    oldyellowRing.style.display = "none";
+
+    var oldorangeRing = document.getElementById("orange");
+    oldorangeRing.style.display = "none";
+
+    var oldlGreenRing = document.getElementById("lGreen");
+    oldlGreenRing.style.display = "none";
+
+    var oldgreenRing = document.getElementById("green");
+    oldgreenRing.style.display = "none";
+
+    var oldblueRing = document.getElementById("blue");
+    oldblueRing.style.display = "none";
+
+    var olddBlueRing = document.getElementById("dBlue");
+    olddBlueRing.style.display = "none";
+
+    var oldvioletRing = document.getElementById("violet");
+    oldvioletRing.style.display = "none";
+
+    var oldpinkRing = document.getElementById("pink");
+    oldpinkRing.style.display = "none";
+
+    originalButton.addEventListener('click', get_mood)
+}
   
 function generate_color() {
 	var answers_length = ringColors.length;
