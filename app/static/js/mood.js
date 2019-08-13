31cae5f06214594401b3077a77aed1e30f66b513
kristin-kernler <34021742+kristin-kernler@users.noreply.github.com>
2019-08-12 18:00:36 -0600

    Added more text

--- app/static/js/mood.js
+++ app/static/js/mood.js
@@ -1,15 +1,5 @@
 const ringColors = ["Black", "Yellow", "Orange", "Light Green", "Green", "Blue", "Dark Blue", "Violet", "Pink"];
 
-// You feel angry, fearful, or apathetic.
-// Your emotions are mixed or conflicted. You may feel distracted or slightly anxious.
-// You are upset. You feel nervous, stressed, or confused about something.
-// You are anxious about something or someone. You are guarded and alert. You may feel mild jealousy or stress.
-// This is the default color. Your emotions are stable today: you do not feel execessive negativity or positivity.
-// You are cheerful and optimistic today
-// You are content.
-// You have romance on your mind. You may feel moody, mischievous or sensual. You are prone to unpredictable choices today.
-// You are overcome with strong emotions. You are infatutated with someone or something.
-
 window.onload = function() {
     answerButton = document.querySelector(".checkButton");
     answerButton.addEventListener('click', get_mood)
@@ -37,7 +27,7 @@ function get_mood() {
     {
         var blackRing = document.getElementById("black");
         blackRing.style.display = "block"; 
-        meaning.textContent = "You feel angry, fearful, or apathetic.";
+        meaning.textContent = "You feel angry, fearful, or apathetic. You may be overworked or depressed.";
     }
     // Yellow
     if (answer.textContent == ringColors[1])
@@ -65,21 +55,21 @@ function get_mood() {
     {
         var greenRing = document.getElementById("green");
         greenRing.style.display = "block";
-        meaning.textContent = "This is the default color. Your emotions are stable today: you do not feel execessive negativity or positivity.";
+        meaning.textContent = "Relaxed. Your emotions are stable today: you do not feel execessive negativity or positivity.";
     }
     // Blue
     if (answer.textContent == ringColors[5])
     {
         var blueRing = document.getElementById("blue");
         blueRing.style.display = "block";
-        meaning.textContent = "You are cheerful and optimistic.";
+        meaning.textContent = "Your mood is upbeat. You are in positive spirits and feel optimistic and amiable.";
     }
     // Dark Blue
     if (answer.textContent == ringColors[6])
     {
         var dBlueRing = document.getElementById("dBlue");
         dBlueRing.style.display = "block";
-        meaning.textContent = "You are content.";
+        meaning.textContent = "You are content. Your inner optimism has been fulfilled and you feel satisfied.";
     }
     // Violet
     if (answer.textContent == ringColors[7])
@@ -93,15 +83,13 @@ function get_mood() {
     {
         var pinkRing = document.getElementById("pink");
         pinkRing.style.display = "block";
-        meaning.textContent = "You are overcome with strong emotions. You are infatuated with someone or something.";
+        meaning.textContent = "You are overcome with strong emotions. You are infatuated with someone or curious about something.";
     }
-
 }
 
 function reset() {
     var answer = document.getElementById("colorText");
     answer.textContent = "";
-
 
     var meaning = document.getElementById("colorMeaning");
     meaning.textContent = "";
@@ -148,8 +136,5 @@ function reset() {
 function generate_color() {
 	var answers_length = ringColors.length;
 	var random_answer_index = Math.floor(Math.random() * answers_length) 
-	//console.log(answers_length)
-	//console.log(random_answer_index)
-	//console.log(mystic_answers[random_answer_index])
 	return ringColors[random_answer_index]
 }
